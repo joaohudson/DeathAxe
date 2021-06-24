20df1a07f80173586287e4a145683295fb60e2b2
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-06-24 16:47:14 -0300

    Refatorando.

--- Assets/Scripts/CharacterAudio.cs
+++ Assets/Scripts/CharacterAudio.cs
@@ -8,20 +8,15 @@ public class CharacterAudio : MonoBehaviour
     [SerializeField]
     private AudioSource source;
     [SerializeField]
-    private AudioClip hitedSound;
-    [SerializeField]
     private AudioClip diedSound;
     [SerializeField]
     private AudioClip attackSound;
-    [SerializeField]
-    private AudioClip bloodSound;
     // Start is called before the first frame update
     void Start()
     {
         var stats = GetComponent<CharacterStats>();
         var combat = GetComponent<CombatController>();
             
-        stats.OnDamage += OnDamage;
         stats.OnDeath += OnDeath;
         combat.OnAttack += OnAttack;
     }
@@ -36,11 +31,5 @@ public class CharacterAudio : MonoBehaviour
     {
         source.clip = diedSound;
         source.Play();
-    }
-
-    private void OnDamage(DamageEvent e)
-    {
-        //source.clip = hitedSound;
-        //source.Play();
     }
 }
